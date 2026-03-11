a0f1f765a66d636e1e8abb927cdee005e51f29c8
myaos-dev <wsteam60@gmail.com>
2026-03-11 20:23:49 +0300

    fixed numbers being messed up

--- kernel/graphics.c
+++ kernel/graphics.c
@@ -6,8 +6,8 @@
 void draw_symbol(boot_info_t* boot, int xcord, int ycord) {
     framebuffer_t* fb = &boot->fb;
     volatile uint32_t* pixels = (volatile uint32_t*)(uintptr_t)fb->base;
-    for (uint32_t y = 19 * (ycord-1); y < 19 * ycord; y++) {
-        for (uint32_t x = 8 * (xcord-1); x < 8 * xcord; x++) {
+    for (uint32_t y = 18 * (ycord-1); y < 18 * ycord; y++) {
+        for (uint32_t x = 9 * (xcord-1); x < 9 * xcord; x++) {
             pixels[y * fb->pixels_per_scanline + x] = 0x00000000;
         }
     }
