4bedaf5ef7c5d34e348537d3e16e822a547bb385
mvmmvmmv22 <165459756+mvmmvmmv22@users.noreply.github.com>
2026-03-11 09:30:45 +0300

    Added symbol coordinates

--- kernel/graphics.c
+++ kernel/graphics.c
@@ -3,15 +3,12 @@
 #include <stddef.h>
 
 
-void write_symbol(boot_info_t* boot) {
+void write_symbol(boot_info_t* boot, int xcord, int ycord) {
     framebuffer_t* fb = &boot->fb;
     volatile uint32_t* pixels = (volatile uint32_t*)(uintptr_t)fb->base;
-    for (uint32_t y = 0; y < 8; y++) {
-        for (uint32_t x = 0; x < 19; x++) {
+    for (uint32_t y = ycord; y < 8 + ycord; y++) {
+        for (uint32_t x = xcord; x < 19 + xcord; x++) {
             pixels[y * fb->pixels_per_scanline + x] = 0x00000000;
         }
-    }
-    for (;;) {
-        __asm__ __volatile__("cli; hlt");
     }
 }
