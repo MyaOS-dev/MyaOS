c2d0842b406c0eadc22a7bf672a27668fb396470
MyaOS-dev <wsteam60@gmail.com>
2026-03-11 19:47:39 +0300

    fixed name

--- kernel/graphics.c
+++ kernel/graphics.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 
 
-void write_symbol(boot_info_t* boot, int xcord, int ycord) {
+void draw_symbol(boot_info_t* boot, int xcord, int ycord) {
     framebuffer_t* fb = &boot->fb;
     volatile uint32_t* pixels = (volatile uint32_t*)(uintptr_t)fb->base;
     for (uint32_t y = ycord; y < 8 + ycord; y++) {
